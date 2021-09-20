b64397899e435b08955407dbc294289d3e47f862
Andrey Senyaev <76472231+asenyaev@users.noreply.github.com>
2021-09-21 01:54:46 +0300

    Try to fix one more time

--- travis_config.sh
+++ travis_config.sh
@@ -39,7 +39,7 @@ if [ -n "$IS_OSX" ]; then
   brew install lapack
   whereis lapack
 else
-  sudo apt-get install libblas-dev liblapack-dev
+  sudo apt-get install libblas-dev liblapack-dev liblapacke-dev
   whereis lapack
 fi
 
