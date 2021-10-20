2ccc5ea9080d54d2b45bb7ad0d59b48bae47f58a
Andrey Senyaev <76472231+asenyaev@users.noreply.github.com>
2021-10-20 12:46:25 +0300

    Lapack lib for macos

--- travis_config.sh
+++ travis_config.sh
@@ -99,6 +99,7 @@ function pre_build {
     brew install lapack
     export LDFLAGS="-L/opt/homebrew/opt/lapack/lib"
     export CPPFLAGS="-I/opt/homebrew/opt/lapack/include"
+    export PKG_CONFIG_PATH="/usr/local/opt/lapack/lib/pkgconfig"
   else
     # epel-release need for aarch64 to get openblas packages
     yum install -y lapack-devel epel-release && yum install -y openblas-devel
